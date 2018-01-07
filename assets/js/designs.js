de895659a98aa27c1ad6bbd74dbd76a7f057a4bb
Cedric-F <31830412+Cedric-F@users.noreply.github.com>
2018-01-07 21:14:06 +0100

    Pixel Art

---
+++ assets/js/designs.js
@@ -0,0 +1,39 @@
+let height, width, row, col, color_new;
+let size = document.getElementById('sizePicker');
+let table = document.getElementById('pixel_canvas');
+let colorPicker = document.getElementById('colorPicker');
+let color = "#000";
+
+
+
+function makeGrid(evt) {
+	evt.preventDefault();
+    table.innerHTML = "";
+	height = document.getElementById('input_height').value;
+	width = document.getElementById('input_width').value;
+
+
+	for (let i = 0; i < height; i++) {
+		row = document.createElement('tr');
+		for (let j = 0; j < width; j++) {
+			col = document.createElement('td');
+			row.appendChild(col);
+		}
+		table.appendChild(row);
+	}
+
+	let cell = document.getElementsByTagName("td");
+	for (var i = 0; i < cell.length; i++) {
+		cell[i].addEventListener('click', cellColor);
+	}
+}
+
+function cellColor() {
+	this.style.backgroundColor = color;
+}
+
+colorPicker.addEventListener('change', function(){
+	color = colorPicker.value;
+});
+
+size.addEventListener('submit', makeGrid);
