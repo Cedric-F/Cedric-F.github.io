6476160d932773bc54ca9a93c54485b566711d2a
Cedric-F <31830412+Cedric-F@users.noreply.github.com>
2018-01-08 19:57:01 +0100

    Feature: Eraser

--- assets/js/designs.js
+++ assets/js/designs.js
@@ -1,11 +1,28 @@
-let height, width, row, col;
-let color = "#000";
+"use strict";
+
+let height, width, row, col, current_color, new_color;
 const create = document.getElementById('creation-form');
 const clear = document.getElementById('clear');
 const table = document.getElementById('pixel_canvas');
 const colorPicker = document.getElementById('colorPicker');
 const cell = document.getElementsByTagName("td");
 
+const hexToRGB = hex => {
+	let r, g, b;
+    r = parseInt(hex.substr(1,2),16);
+    g = parseInt(hex.substr(3,2),16);
+    b = parseInt(hex.substr(5,2),16);
+    return `rgb(${r}, ${g}, ${b})`;
+  }
+
+const cellColor = evt => {
+	current_color = evt.currentTarget.style.backgroundColor;
+	new_color = hexToRGB(colorPicker.value);
+
+	current_color !== new_color
+	? evt.currentTarget.style.backgroundColor = new_color
+	: evt.currentTarget.removeAttribute('style');
+}
 
 const makeGrid = evt => {
 	evt.preventDefault();
@@ -35,28 +52,6 @@ const clearGrid = _ => {
 		}
 	}
 }
-
-const hexToRGB = hex => {
-	let r, g, b;
-	let result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-	r = parseInt(result[1], 16);
-    g = parseInt(result[2], 16);
-    b = parseInt(result[3], 16);
-    return `rgb(${r}, ${g}, ${b})`;
-}
-
-const cellColor = evt => {
-	evt.currentTarget.setAttribute('style', 'background-color:' + color);
-}
-
-const removeColor = evt => {
-	let current = evt.currentTarget.hasAttribute('style');
-	console.log(current);
-}
-
-colorPicker.addEventListener('change', () => {
-	color = colorPicker.value;
-});
 
 create.addEventListener('submit', makeGrid);
 clear.addEventListener('click', clearGrid);
