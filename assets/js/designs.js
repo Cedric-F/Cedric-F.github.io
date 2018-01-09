7add50e784d1880128d870ac4121362caeca5c47
Cedric-F <31830412+Cedric-F@users.noreply.github.com>
2018-01-09 18:34:08 +0100

    Feature: Click and Drag option

--- assets/js/designs.js
+++ assets/js/designs.js
@@ -1,6 +1,7 @@
 "use strict";
 
 let height, width, row, col, current_color, new_color;
+let drag = false;
 const create = document.getElementById('creation-form');
 const clear = document.getElementById('clear');
 const table = document.getElementById('pixel_canvas');
@@ -8,25 +9,26 @@ const colorPicker = document.getElementById('colorPicker');
 const cell = document.getElementsByTagName("td");
 
 const hexToRGB = hex => {
-	let r, g, b;
-    r = parseInt(hex.substr(1,2),16);
-    g = parseInt(hex.substr(3,2),16);
-    b = parseInt(hex.substr(5,2),16);
-    return `rgb(${r}, ${g}, ${b})`;
+	let rgb = {
+		'r' : parseInt(hex.substr(1,2),16),
+		'g' : parseInt(hex.substr(3,2),16),
+		'b' : parseInt(hex.substr(5,2),16)
+	};
+    return `rgb(${rgb.r}, ${rgb.g}, ${rgb.b})`; // convert the hex color in the "rgb(r, g, b)" format. e.g: #ffffff -> 255, 255, 255
   }
 
-const cellColor = evt => {
-	current_color = evt.currentTarget.style.backgroundColor;
-	new_color = hexToRGB(colorPicker.value);
+  const colors = c => { // c for "cell"
+  	current_color = c.target.style.backgroundColor; // get the current color of the cell
+	new_color = hexToRGB(colorPicker.value); // convert the selected color in rgb
 
-	(current_color !== new_color)
-	? evt.currentTarget.style.backgroundColor = new_color
-	: evt.currentTarget.removeAttribute('style');
-}
+	(current_color !== new_color) // if the two colors don't match (are different)
+	? c.target.style.backgroundColor = new_color // apply new color to the cell(s)
+	: c.target.removeAttribute('style'); // else remove the color
+  }
 
 const makeGrid = evt => {
 	evt.preventDefault();
-    table.innerHTML = "";
+    table.innerHTML = ""; // empty the table content
 	height = document.getElementById('input_height').value;
 	width = document.getElementById('input_width').value;
 
@@ -40,18 +42,35 @@ const makeGrid = evt => {
 		table.appendChild(row);
 	}
 
-	for (let i = 0; i < cell.length; i++) {
-		cell[i].addEventListener('click', cellColor);
+	for (let i = 0; i < cell.length; i++) { // add the following eventListeners to all the cells
+		cell[i].addEventListener('mousedown', e => { // when holding mouse click
+			drag = true; // enable drag option
+			console.log('mousedown');
+			colors(e); // color the cell
+		});
+		cell[i].addEventListener('mouseup', _ => { // when releasing the mouse click
+			console.log('mouseup');
+			drag = false; // disable drag option
+		});
+		cell[i].addEventListener('mouseover', e => { // when hovering over the cells
+			if (drag) { // if the drag option is enabled (mousedown) is true then
+				console.log('mouseover');
+				colors(e); // color the cells
+			}
+		});
 	}
 }
 
 const clearGrid = _ => {
-	for (let i = 0; i < cell.length; i++) {
-		if (cell[i].hasAttribute('style')){
-			cell[i].removeAttribute('style');
+	for (let i = 0; i < cell.length; i++) { // navigate through each cell
+		if (cell[i].hasAttribute('style')){ // if a cell has a color then
+			cell[i].removeAttribute('style'); // remove it
 		}
 	}
 }
 
-create.addEventListener('submit', makeGrid);
-clear.addEventListener('click', clearGrid);
+create.addEventListener('submit', makeGrid); // create the grid when submitting the form
+clear.addEventListener('click', clearGrid); // clear the grid (remove colors)
+table.addEventListener('mouseleave', _ => { // hover outside the grid will cancel the drag option
+	drag = false;
+});
