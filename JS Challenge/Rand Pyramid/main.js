9422fb406a1a1fbf6de3f612f3b1a89042b5b4a8
Cedric-F <31830412+Cedric-F@users.noreply.github.com>
2018-01-13 04:52:24 +0100

    Doc: Comments

--- JS Challenge/Rand Pyramid/main.js
+++ JS Challenge/Rand Pyramid/main.js
@@ -4,26 +4,26 @@ let arr = document.getElementById('array');
 let pyramid = document.getElementById('sort');
 
 const makeArray = e => {
-	rows = document.getElementById('row').value;
-	e.preventDefault();
-	arr.innerHTML = '';
+	rows = document.getElementById('row').value; // get rows amount
+	e.preventDefault(); // no reload
+	arr.innerHTML = ''; // clean the existing results, if there is any
 	pyramid.innerHTML = '';
-	line = Array();
+	line = Array(); // Create an empty array
 	for (let i = 1; i <= rows; i++) {
-		line.push([... Array(i)]);
+		line.push([... Array(i)]); // add array of index i and i empty elements
 	}
-	return add(line.map(e => e.map((e, i) => e = Math.floor(Math.random() * (9-1)+1))));
+	return add(line.map(e => e.map((e, i) => e = Math.floor(Math.random() * (9-1)+1)))); // assign a random value (from 1 to 9) to each sub element
 }
 
 const add = n => {
 	console.log(n);
 	let total = 0;
-	for (let i = 0; i < n.length; i++) {
+	for (let i = 0; i < n.length; i++) { // sum of all min numbers
 		total += Math.min(...n[i]);
 		console.log(Math.min(...n[i]));
 	}
 
-	document.getElementById('sort').appendChild(makeUl(n));
+	document.getElementById('sort').appendChild(makeUl(n)); // creates the list
 	arr.innerHTML = `The solution is ${total}`;
 }
 
@@ -32,7 +32,7 @@ const makeUl = n => {
 	let item;
 	for (let i = 0; i < n.length; i++) {
 		item = document.createElement('li');
-		item.appendChild(document.createTextNode(n[i]));
+		item.appendChild(document.createTextNode(n[i])); // each line gets an array
 		list.appendChild(item);
 	}
 	return list;
