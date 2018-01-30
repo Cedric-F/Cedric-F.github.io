99883396ed9021327ef46ac6b7db1e3f11c31ae1
Cedric-F <31830412+Cedric-F@users.noreply.github.com>
2018-01-30 01:48:16 +0100

    Miscellaneous

--- JS Challenge/Rand Pyramid/main.js
+++ JS Challenge/Rand Pyramid/main.js
@@ -4,39 +4,39 @@ let arr = document.getElementById('array');
 let pyramid = document.getElementById('sort');
 
 const makeArray = e => {
-	rows = document.getElementById('row').value; // get rows amount
-	e.preventDefault(); // no reload
-	arr.innerHTML = ''; // clean the existing results, if there is any
-	pyramid.innerHTML = '';
-	line = Array(); // Create an empty array
-	for (let i = 1; i <= rows; i++) {
-		line.push([... Array(i)]); // add array of index i and i empty elements
-	}
-	return add(line.map(e => e.map((e, i) => e = Math.floor(Math.random() * (10-1)+1)))); // assign a random value (from 1 to 9) to each sub element
+  rows = document.getElementById('row').value; // get rows amount
+  e.preventDefault(); // no reload
+  arr.innerHTML = ''; // clean the existing results, if there is any
+  pyramid.innerHTML = '';
+  line = Array(); // Create an empty array
+  for (let i = 1; i <= rows; i++) {
+    line.push([... Array(i)]); // add array of index i and i empty elements
+  }
+  return add(line.map(e => e.map((e, i) => e = Math.floor(Math.random() * (10-1)+1)))); // assign a random value (from 1 to 9) to each sub element
 }
 
 const add = n => {
-	console.log(n);
-	let total = 0;
-	for (let i = 0; i < n.length; i++) { // sum of all min numbers
-		total += Math.min(...n[i]);
-		console.log(Math.min(...n[i]));
-	}
+  console.log(n);
+  let total = 0;
+  for (let i = 0; i < n.length; i++) { // sum of all min numbers
+    total += Math.min(...n[i]);
+    console.log(Math.min(...n[i]));
+  }
 
-	document.getElementById('sort').appendChild(makeUl(n)); // creates the list
-	arr.innerHTML = `The solution is ${total}`;
-	console.log(`The solution is ${total}`);
+  document.getElementById('sort').appendChild(makeUl(n)); // creates the list
+  arr.innerHTML = `The solution is ${total}`;
+  console.log(`The solution is ${total}`);
 }
 
 const makeUl = n => {
-	let list = document.createElement('ul');
-	let item;
-	for (let i = 0; i < n.length; i++) {
-		item = document.createElement('li');
-		item.appendChild(document.createTextNode(n[i])); // each line gets an array
-		list.appendChild(item);
-	}
-	return list;
+  let list = document.createElement('ul');
+  let item;
+  for (let i = 0; i < n.length; i++) {
+    item = document.createElement('li');
+    item.appendChild(document.createTextNode(n[i])); // each line gets an array
+    list.appendChild(item);
+  }
+  return list;
 }
 
 
--- assets/js/designs.js
+++ assets/js/designs.js
@@ -18,58 +18,58 @@ const audio = document.getElementById('fusrohdah');
 /* Functions */
 
 const dye = e => { // colors the cells. if the eraser option is checked, cleanse them instead
-	if (!eraser.checked) {
-		color = colorPicker.value;
-		e.target.style.backgroundColor = color;
-	} else {
-		e.target.removeAttribute('style');
-	}
+  if (!eraser.checked) {
+    color = colorPicker.value;
+    e.target.style.backgroundColor = color;
+  } else {
+    e.target.removeAttribute('style');
+  }
 };
 
 const makeGrid = e => { // add the desired amount of rows and cells to the table.
-	e.preventDefault();
-	fusrohdah.play();
-	table.innerHTML = ""; // empty the table content
-	height = document.getElementById('input_height').value;
-	width = document.getElementById('input_width').value;
+  e.preventDefault();
+  fusrohdah.play();
+  table.innerHTML = ""; // empty the table content
+  height = document.getElementById('input_height').value;
+  width = document.getElementById('input_width').value;
 
-	for (let i = 0; i < height; i++) {
-		row = table.insertRow(i);
-		for (let j = 0; j < width; j++) {
-			row.insertCell(j);
-		}
-	}
+  for (let i = 0; i < height; i++) {
+    row = table.insertRow(i);
+    for (let j = 0; j < width; j++) {
+      row.insertCell(j);
+    }
+  }
 
-	document.getElementById('box').style.visibility = "visible";
+  document.getElementById('box').style.visibility = "visible";
   toggleBorders.innerHTML = "Disable";
   border.checked = false;
 
-	for (let i = 0; i < cell.length; i++) { // add the following eventListeners to all the cells
-		cell[i].addEventListener('mousedown', e => { // when holding mouse click, enable drag option and color the clicked cell
-			console.log('mousedown');
-			e.preventDefault();
-			drag = true;
-			dye(e);
-		});
-		cell[i].addEventListener('mouseup', _ => { // when releasing the mouse click, disable the drag option
-			console.log('mouseup');
-			drag = false;
-		});
-		cell[i].addEventListener('mouseover', e => { // when hovering over the cells, while mousedown is active, color them
-			if (drag) {
-				console.log('mouseover');
-				dye(e);
-			}
-		});
-	}
+  for (let i = 0; i < cell.length; i++) { // add the following eventListeners to all the cells
+    cell[i].addEventListener('mousedown', e => { // when holding mouse click, enable drag option and color the clicked cell
+      console.log('mousedown');
+      e.preventDefault();
+      drag = true;
+      dye(e);
+    });
+    cell[i].addEventListener('mouseup', _ => { // when releasing the mouse click, disable the drag option
+      console.log('mouseup');
+      drag = false;
+    });
+    cell[i].addEventListener('mouseover', e => { // when hovering over the cells, while mousedown is active, color them
+      if (drag) {
+        console.log('mouseover');
+        dye(e);
+      }
+    });
+  }
 };
 
 const clearGrid = _ => { // Navigate through each cell to cleanse the colored ones
-	for (let i = 0; i < cell.length; i++) {
-		if (cell[i].hasAttribute('style')) {
-			cell[i].removeAttribute('style');
-		}
-	}
+  for (let i = 0; i < cell.length; i++) {
+    if (cell[i].hasAttribute('style')) {
+      cell[i].removeAttribute('style');
+    }
+  }
 };
 
 /* Global event listeners */
@@ -77,19 +77,19 @@ const clearGrid = _ => { // Navigate through each cell to cleanse the colored on
 create.addEventListener('submit', makeGrid); // create the grid when submitting the form
 clear.addEventListener('click', clearGrid); // clear the grid (cleanse colored cells)
 table.addEventListener('mouseleave', _ => { // hover outside the grid will cancel the drag option
-	drag = false;
+  drag = false;
 });
 table.addEventListener('contextmenu', e => { // cancels right click default behavior for the grid (no context menu)
-	e.preventDefault();
+  e.preventDefault();
 });
 border.addEventListener('change', _ => { // Toggle the visibility of the cells borders
-	if (border.checked) {
-		toggleBorders.innerHTML = "Enable";
-		[].map.call(document.querySelectorAll('tr'), e => e.classList.toggle('border'));
-		[].map.call(document.querySelectorAll('td'), e => e.classList.toggle('border'));
-	} else {
-		toggleBorders.innerHTML = "Disable";
-		[].map.call(document.querySelectorAll('tr'), e => e.classList.remove('border'));
-		[].map.call(document.querySelectorAll('td'), e => e.classList.remove('border'));
-	}
+  if (border.checked) {
+    toggleBorders.innerHTML = "Enable";
+    [].map.call(document.querySelectorAll('tr'), e => e.classList.toggle('border'));
+    [].map.call(document.querySelectorAll('td'), e => e.classList.toggle('border'));
+  } else {
+    toggleBorders.innerHTML = "Disable";
+    [].map.call(document.querySelectorAll('tr'), e => e.classList.remove('border'));
+    [].map.call(document.querySelectorAll('td'), e => e.classList.remove('border'));
+  }
 });
