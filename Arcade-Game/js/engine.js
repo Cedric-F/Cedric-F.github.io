fba7c9d0c34f7a8e23b8e1bccc42670a0b051b13
Cedric F <cedric.fontaine@gmx.fr>
2018-05-11 17:07:12 +0200

    Added patrolling entity. Testing unit

--- Arcade-Game/js/engine.js
+++ Arcade-Game/js/engine.js
@@ -1,84 +1,93 @@
-let Engine = ((global) => {
-    let win = global.window,
-        lastTime;
+let engine = ((global) => {
+  let win = global.window,
+      lastTime;
 
-    const main = () => {
-        let now = Date.now(),
-            dt = (now - lastTime) / 1000.0;
+  const main = () => {
+    let now = Date.now(),
+        dt = (now - lastTime) / 1000.0;
 
-        /* Call our update/render functions, pass along the time delta to
-         * our update function since it may be used for smooth animation.
-         */
-        update(dt);
-        render();
-
-        /* Set our lastTime variable which is used to determine the time delta
-         * for the next time this function is called.
-         */
-        lastTime = now;
-
-        /* Use the browser's requestAnimationFrame function to call this
-         * function again as soon as the browser is able to draw another frame.
-         */
-        win.requestAnimationFrame(main);
-    };
-
-    /* This function does some initial setup that should only occur once,
-     * particularly setting the lastTime variable that is required for the
-     * game loop.
+    /* Call our update/render functions, pass along the time delta to
+     * our update function since it may be used for smooth animation.
      */
-    const init = () => {
-        lastTime = Date.now();
-        main();
-    };
+    update(dt);
+    render();
 
-    /* This function is called by main (our game loop) and itself calls all
-     * of the functions which may need to update entity's data. Based on how
-     * you implement your collision detection (when two entities occupy the
-     * same space, for instance when your character should die), you may find
-     * the need to add an additional function call here. For now, we've left
-     * it commented out - you may or may not want to implement this
-     * functionality this way (you could just implement collision detection
-     * on the entities themselves within your app.js file).
+    /* Set our lastTime variable which is used to determine the time delta
+     * for the next time this function is called.
      */
-    const update = (dt) => updateEntities(dt);
+    lastTime = now;
 
-    /* This is called by the update function and loops through all of the
-     * objects within your allEnemies array as defined in app.js and calls
-     * their update() methods. It will then call the update function for your
-     * player object. These update methods should focus purely on updating
-     * the data/properties related to the object. Do your drawing in your
-     * render methods.
+    /* Use the browser's requestAnimationFrame function to call this
+     * function again as soon as the browser is able to draw another frame.
      */
-    const updateEntities = (dt) => player.update();
+    win.requestAnimationFrame(main);
+  };
 
-    /* This function initially draws the "game level", it will then call
-     * the renderEntities function. Remember, this function is called every
-     * game tick (or loop of the game engine) because that's how games work -
-     * they are flipbooks creating the illusion of animation but in reality
-     * they are just drawing the entire screen over and over.
+  /* This function does some initial setup that should only occur once,
+   * particularly setting the lastTime variable that is required for the
+   * game loop.
+   */
+  const init = () => {
+    lastTime = Date.now();
+    main();
+  };
+
+  /* This function is called by main (our game loop) and itself calls all
+   * of the functions which may need to update entity's data. Based on how
+   * you implement your collision detection (when two entities occupy the
+   * same space, for instance when your character should die), you may find
+   * the need to add an additional function call here. For now, we've left
+   * it commented out - you may or may not want to implement this
+   * functionality this way (you could just implement collision detection
+   * on the entities themselves within your app.js file).
+   */
+  const update = (dt) => updateEntities(dt);
+
+  /* This is called by the update function and loops through all of the
+   * objects within your allEnemies array as defined in app.js and calls
+   * their update() methods. It will then call the update function for your
+   * player object. These update methods should focus purely on updating
+   * the data/properties related to the object. Do your drawing in your
+   * render methods.
+   */
+  const updateEntities = (dt) => {
+    allEnemies.forEach(enemy => enemy.update(dt));
+    player.update();
+  };
+
+  /* This function initially draws the "game level", it will then call
+   * the renderEntities function. Remember, this function is called every
+   * game tick (or loop of the game engine) because that's how games work -
+   * they are flipbooks creating the illusion of animation but in reality
+   * they are just drawing the entire screen over and over.
+   */
+  const render = () => {
+    maze.ctx.clearRect(0,0, maze.width, maze.height)
+    maze.draw();
+    renderEntities();
+  };
+
+  /* This function is called by the render function and is called on each game
+   * tick. Its purpose is to then call the render functions you have defined
+   * on your enemy and player entities within app.js
+   */
+  const renderEntities = () => {
+    /* Loop through all of the objects within the allEnemies array and call
+     * the render function you have defined.
      */
-    const render = () => {
-      maze.ctx.clearRect(0,0, maze.width, maze.height)
-      maze.ctx.drawImage(maze.img, 0, 0);
-      renderEntities();
-    };
+    allEnemies.forEach(enemy => enemy.render());
+    player.update();
+  };
 
-    /* This function is called by the render function and is called on each game
-     * tick. Its purpose is to then call the render functions you have defined
-     * on your enemy and player entities within app.js
-     */
-    const renderEntities = () => player.update();
+  /* Go ahead and load all of the images we know we're going to need to
+   * draw our game level. Then set init as the callback method, so that when
+   * all of these images are properly loaded our game will start.
+   */
+  Resources.load([
+    'resources/maze.png',
+    'resources/player.png'
+  ]);
 
-    /* Go ahead and load all of the images we know we're going to need to
-     * draw our game level. Then set init as the callback method, so that when
-     * all of these images are properly loaded our game will start.
-     */
-    Resources.load([
-      'resources/maze.png',
-      'resources/player.png'
-    ]);
-
-    Resources.onReady(init);
+  Resources.onReady(init);
 
 })(this);
--- Arcade-Game/js/maze.js
+++ Arcade-Game/js/maze.js
@@ -32,17 +32,17 @@ class Player {
         } else if (nX < this.x) {
           return maze.ctx.getImageData(nX, nY, 15, 15);
         } else if (nY > this.y) {
-          return maze.ctx.getImageData(nX, nY + 8.5, 15, 15);
+          return maze.ctx.getImageData(nX, nY + 10, 15, 15);
         } else if (nY < this.y) {
           return maze.ctx.getImageData(nX, nY, 15, 15);
         }
       })(),
-        data = imgData.data,
+        data = imgData.data.filter((e, i) => !(i % 4)),
         collision = false;
     console.log(data);
 
     if (!(nX >= 0 && nX <= maze.width - 25 && nY >= 0 && nY <= maze.height - 31)) collision = true; // Prevents from going off canvas
-    for (let i = 0; i < 4 * 25 * 31; i += 4) {
+    for (let i = 0; i < 15 * 15; i++) {
       if (data[i] === 0) {
         collision = true;
         break;
@@ -71,19 +71,87 @@ class Player {
         nX = this.x;
         nY = this.y + 22.5;
         break;
+      default: return;
     }
+
     if (this.checkCollision(nX, nY)) return; // if collision is true, don't move
+
     this.x = nX;
     this.y = nY;
     this.update();
+    console.log(this);
   }
-
 }
 
+class Monster {
+  constructor(x, y, w, h, dir, sprite) {
+    this.x = x;
+    this.y = y;
+    this.w = 22;
+    this.h = 31;
+    this.sprite = 'resources/player.png';
+    this.dir = ~~(Math.random() * 2);
+  }
+
+  update(dt) {
+    let nX = dt * 30;
+    /*if (this.checkCollision(nX)){
+      if (this.dir == 1) {
+        this.dir = 0;
+      } else if (this.dir == 0) {
+        this.dir = 1;
+      }
+    }*/
+    if (this.dir) {
+      if (!this.checkCollision(nX)) {
+        this.x += nX;
+      } else {
+        this.dir = 0;
+      }
+    } else {
+      if (!this.checkCollision(nX)) {
+        this.x -= nX;
+      } else {
+        this.dir = 1;
+      }
+    }
+    /*
+     * Check the direction, then if a collision occurs in this direction.
+     * If it does, change the direction.
+     */
+  }
+
+  render() {
+    maze.ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  }
+
+  checkCollision(nX) {
+    let imgData = (() =>
+      this.dir ?
+      maze.ctx.getImageData(this.x + this.w + nX, this.y, 15, 15) :
+      maze.ctx.getImageData(this.x - nX, this.y, 15, 15)
+      /*if (this.dir == 1 && nX > this.x) {
+        return maze.ctx.getImageData(nX + 2.5, this.y, 15, 15);
+      } else if (this.dir == 0 && nX < this.x) {
+        return maze.ctx.getImageData(nX, this.y, 15, 15);
+      }*/
+    )(),
+        data = imgData.data.filter((e, i) => !(i % 4)),
+        collision = false;
+
+    for (let i = 0; i < 15 * 15; i++) {
+      if (!data[i] && !data[i+1] && !data[i+2]) { // if a pixel is black, there is a collision
+        collision = true;
+        break;
+      }
+    }
+    return collision;
+  }
+}
 
 const player = new Player();
 const maze = new Maze();
-
+const allEnemies = [new Monster(20, 10)];
 
 
 document.addEventListener('keydown', function(e) {
