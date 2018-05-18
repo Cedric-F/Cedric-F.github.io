9e4d3ff63d6e55e62d012b078029d33b22fabebf
Cedric F <31830412+Cedric-F@users.noreply.github.com>
2018-05-18 14:04:33 +0200

    Update app.js
    Changed links protocols

--- Feedreader-Testing/js/app.js
+++ Feedreader-Testing/js/app.js
@@ -10,19 +10,19 @@
 var allFeeds = [
   {
     name: "Udacity Blog",
-    url: "http://blog.udacity.com/feed"
+    url: "https://blog.udacity.com/feed"
   },
   {
     name: "CSS Tricks",
-    url: "http://feeds.feedburner.com/CssTricks"
+    url: "https://feeds.feedburner.com/CssTricks"
   },
   {
     name: "HTML5 Rocks",
-    url: "http://feeds.feedburner.com/html5rocks"
+    url: "https://feeds.feedburner.com/html5rocks"
   },
   {
     name: "Linear Digressions",
-    url: "http://feeds.feedburner.com/udacity-linear-digressions"
+    url: "https://feeds.feedburner.com/udacity-linear-digressions"
   }
 ];
 
