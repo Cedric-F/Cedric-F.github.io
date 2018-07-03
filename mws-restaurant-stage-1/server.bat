4928aa9c6af77472a12d977b6c88f42df5e90a51
Cedric F <cedric.fontaine@gmx.fr>
2018-07-03 19:44:51 +0200

    Initial commit

---
+++ mws-restaurant-stage-1/server.bat
@@ -0,0 +1,4 @@
+@echo
+python -m http.server 8000
+@echo
+@echo
---
+++ mws-restaurant-stage-1/sw.js
@@ -0,0 +1,31 @@
+self.addEventListener('install', e => {
+	e.waitUntil(
+			caches.open('Restaurant-Reviews-v1.0').then(c => c.addAll(
+					[
+						'/',
+						'index.html',
+						'restaurant.html',
+						'css/styles.css',
+						'data/restaurants.json',
+						'img/1.jpg',
+						'img/2.jpg',
+						'img/3.jpg',
+						'img/4.jpg',
+						'img/5.jpg',
+						'img/6.jpg',
+						'img/7.jpg',
+						'img/8.jpg',
+						'img/9.jpg',
+						'js/dbhelper.js',
+						'js/main.js',
+						'js/restaurant_info.js'
+					]
+			))
+		);
+});
+
+self.addEventListener('fetch', e => {
+	e.respondWith(
+		caches.match(e.request).then(r => r ? r : fetch(e.request))
+	);
+});
