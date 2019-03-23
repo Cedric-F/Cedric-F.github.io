1ba5dbc551aefcd29500e3cd2e8a01a101470533
Cedric F <31830412+Cedric-F@users.noreply.github.com>
2019-03-23 21:07:31 +0100

    Update main.js

--- Miscellaneous/JS-Challenge/Rand-Pyramid/main.js
+++ Miscellaneous/JS-Challenge/Rand-Pyramid/main.js
@@ -1,20 +1,25 @@
+/*
+ * An old exercise on DOM manipulation. Creates a pyramid of random numbers and find the sum of the smallest number of each row
+ */
+
+
 let line,
-		rows = document.getElementById('row'),
-		go = document.getElementById('go'),
-		solution = document.getElementById('solution'),
-		pyramid = document.getElementById('sort');
+    rows = document.getElementById('row'),
+    go = document.getElementById('go'),
+    solution = document.getElementById('solution'),
+    pyramid = document.getElementById('sort');
 
 const makeArray = e => {
   e.preventDefault(); // no reload
 
-  solution.innerHTML = ''; // clean the existing results, if there is any
+  solution.innerHTML = ''; // clear the existing results, if there is any
   pyramid.innerHTML = '';
 
-  line = Array(); // Create an empty array
+  line = Array();
   for (let i = 1; i <= rows.value; i++) {
-    line.push([... Array(i)]); // add array of index i and i empty elements
+    line.push([... Array(i)]); // add an array of index i and i empty elements
   }
-  return add(line.map(e => e.map((e, i) => e = Math.floor(Math.random() * 10)))); // assign a random value (from 1 to 9) to each sub element
+  return add(line.map(e => e.map((e, i) => e = ~~(Math.random() * 10)))); // assign a random value (from 1 to 9) to each sub element
 }
 
 const add = n => {
